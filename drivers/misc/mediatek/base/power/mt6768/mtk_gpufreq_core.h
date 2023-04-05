671de0a55a5bd6f903a64328df5dc3f2f1e4ae19
01soni247 <01soni247@gmail.com>
2023-04-05 13:18:12 +0700

    defconfig: Corruption-Succubus+!

--- drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
+++ drivers/misc/mediatek/base/power/mt6768/mtk_gpufreq_core.h
@@ -17,8 +17,8 @@
 /**************************************************
  * MT6768/MT6767 : GPU DVFS OPP table Setting
  **************************************************/
-#define SEG_GPU_DVFS_FREQ0			(1000000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ1			(975000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ0			(1025000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ1			(1005000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ2			(950000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ3			(925000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ4			(900000)	/* KHz */
@@ -46,9 +46,9 @@
 #define SEG_GPU_DVFS_FREQ26			(383000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ27			(366000)	/* KHz */
 #define SEG_GPU_DVFS_FREQ28			(349000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ29			(332000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ30			(315000)	/* KHz */
-#define SEG_GPU_DVFS_FREQ31			(299000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ29			(325000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ30			(275000)	/* KHz */
+#define SEG_GPU_DVFS_FREQ31			(250000)	/* KHz */
 
 #define SEG_GPU_DVFS_VOLT0		(95000)		/* mV x 100 */
 #define SEG_GPU_DVFS_VOLT1		(92500)		/* mV x 100 */
